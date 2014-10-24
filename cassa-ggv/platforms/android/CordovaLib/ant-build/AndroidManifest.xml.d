e103ea3bcfa0bc47d003bece431819b74f6be8d9
Daniele Votta <vottadaniele@gmail.com>
2014-10-24 19:56:37 +0200

    Predisposizione per app

---
+++ cassa-ggv/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\votta\Desktop\cassa-ggv\cassa-ggv\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\votta\Desktop\cassa-ggv\cassa-ggv\platforms\android\CordovaLib\AndroidManifest.xml \
